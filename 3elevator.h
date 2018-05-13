fd09b4e348a95b5eaeb63f70fc359b1c9da2521b
Highstarrr <36108071+Highstarrr@users.noreply.github.com>
2018-05-13 21:38:10 +0800

    Create 3elevator.h

---
+++ 3elevator.h
@@ -0,0 +1,23 @@
+#ifndef elevator
+
+#define elevator
+
+class elevator
+
+{
+
+	public:
+
+		int currentfloor,indicator;
+
+		queue<int>destination; 
+
+		int gotofloor();
+
+		int stop();
+
+		int updatedestinationqueue();
+
+};
+
+#endif
